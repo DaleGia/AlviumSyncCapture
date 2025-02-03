7d77467d83d5f89f5c85ea3f7f9615b024a5629d
dfn <d.giancono@gmail.com>
2025-02-03 10:04:25 +0800

    Updating install script to work

--- install.sh
+++ install.sh
@@ -1,6 +1,17 @@
+# Installs all of the submodules
 git submodule update --init --recursive
-cd AlliedVisionAlvium/AlliedVisionVimbaX
-bash install.sh
+cd AlliedVisionAlvium
+
+# Installs Allied Vision VimbaX and Camera Drivers
+git clone  git@github.com:DaleGia/AlliedVisionVimbaX.git
+cd AlliedVisionVimbaX
+tar -xzvf VimbaX_Setup-2024-1-Linux64.tar.gz
+sudo cp -r VimbaX_2024-1 /usr/local/lib/VimbaX
+rm -r VimbaX_2024-1/
+sudo sh /usr/local/lib/VimbaX/cti/Install_GenTL_Path.sh
+sudo ldconfig
+
+
 cd ../../
 sudo apt-get install -y g++ cmake xorg-dev libglu1-mesa-dev libstdc++6 libopencv-dev libcfitsio-dev gpsd libgps-dev ntpsec
 mkdir build
@@ -8,5 +19,6 @@ cd build
 cmake ..
 cmake --build .
 cd ..
-cp ntpsec /etc/ntpsec/ntp.conf
+
+cp ntp.conf /etc/ntpsec/ntp.conf
 systemctl restart ntpsec.service
